543032f153c5b973ccfadea628e5fca42269be04
Makot3 <makot.55h@gmail.com>
2017-06-17 13:33:06 +0900

    helo

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  root to: 'kaigyo#index'
+  root 'hello#index'
   get 'hello/index', to: 'hello#index'
 
   get 'kaigyo/index'
